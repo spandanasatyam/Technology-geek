06530dcce243ea29416dd67c7afd7c9474550902
spandanasathyam <spandanasathyam@gmail.com>
2019-02-15 12:53:51 +0530

    Added Executable

--- Framework Implimentaion/src/Tests/LoginTests.java
+++ Framework Implimentaion/src/Tests/LoginTests.java
@@ -1,15 +1,31 @@
 package Tests;
 
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 public class LoginTests {
 
-	public static void main(String args) {
+	
 	WebDriver driver;
-	System.setProperty("webdriver.chrome.driver","C:\\Users\\Spandana.ankathi\\git\\git\\Framework Implimentaion\\DriverExecutables\\chromedriver.exe");
 	
 	
+	@Test
+	public void firstest() throws InterruptedException {
+		
+		System.out.println("Reached here");
+		
+	/*System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\gitRepo\\Framework Implimentaion\\DriverExecutables\\chromedriver.exe");
+	
+	driver=new ChromeDriver();
+	
+	LoginEAGSyne LoginEAGSyneobj=new LoginEAGSyne(driver);
+
+	
+	Thread.sleep(3000);
+	driver.get("https://eag.synechron.com");*/
+	
+	}
 	
 	
 	}
-}
+
